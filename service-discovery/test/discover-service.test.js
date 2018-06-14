2bb78f084b5ceaa4a29a14eaa9678147147d0886
Shalmali85 <33997267+Shalmali85@users.noreply.github.com>
2018-06-14 18:49:21 +1000

    Added test cases

--- service-discovery/test/discover-service.test.js
+++ service-discovery/test/discover-service.test.js
@@ -1,24 +1,32 @@
-const mocha = require('mocha');
 const chai = require('chai');
 const sinon = require('sinon');
 const CfNodejsClient = require('cf-nodejs-client');
-const service = require('../../src/shared/discover-service.js');
-const expect = chai.expect();
+const data = require('./cf.test.json');
+const service = require('../src/discover-service.js');
+
+const expect = chai.expect;
 
 chai.should();
 
 const sandbox = sinon.sandbox.create();
 
 describe('Service Discovery test', () => {
-  let serviceStub;
+  let cloudControllerStub;
+  let usersUAAStub;
+  let appStub;
 
   function setupServiceStubs() {
-    serviceStub.getInfo = sandbox.stub(CfNodejsClient.prototype, 'getInfo');
+    cloudControllerStub.getInfo = sandbox.stub(CfNodejsClient.CloudController.prototype, 'getInfo');
+    usersUAAStub.login = sandbox.stub(CfNodejsClient.UsersUAA.prototype, 'login');
+    appStub.getApps = sandbox.stub(CfNodejsClient.Apps.prototype, 'getApps');
+    appStub.getEnvironmentVariables = sandbox.stub(CfNodejsClient.Apps.prototype, 'getEnvironmentVariables');
   }
 
 
   beforeEach(() => {
-    serviceStub = sandbox.spy(() => new CfNodejsClient());
+    cloudControllerStub = sandbox.spy(() => new CfNodejsClient.CloudController('mockUrl'));
+    usersUAAStub = sandbox.spy(() => new CfNodejsClient.UsersUAA());
+    appStub = sandbox.spy(() => new CfNodejsClient.Apps('mockUrl'));
     setupServiceStubs();
   });
 
@@ -26,12 +34,33 @@ describe('Service Discovery test', () => {
     sandbox.restore();
   });
 
-  it('should return a list with matching services for a pattern', (done) => {
-    const map = new Map('role:test', { name: test, url: 'test.cfapps.io' });
-      serviceStub.getInfo.resolves({ name: 'test', url: 'test.cfapps.io' });
-    const result = service.discover([{ pin: 'role:test' }], { domain: 'mockUrl', name: '***', password: '***' },
+  it('should return a map with matching services for a pattern', () => {
+    cloudControllerStub.getInfo.resolves(data.cfData.infoData);
+    usersUAAStub.login.resolves(data.cfData.loginData);
+    appStub.getApps.resolves(data.cfData.appData);
+    appStub.getEnvironmentVariables.resolves(data.cfData.vcapData[0]);
+    service.discover([{ pin: 'role:test' }], { domain: 'mockUrl', name: '*', password: '*' },
       (err, response) => {
-        expect(response).to.be.equal(map);
+        expect(response).to.be.a('Map');
+      });
+  });
+
+  it('should return a map when no pin exist ', () => {
+    cloudControllerStub.getInfo.resolves(data.cfData.infoData);
+    usersUAAStub.login.resolves(data.cfData.loginData);
+    appStub.getApps.resolves(data.cfData.appData);
+    appStub.getEnvironmentVariables.resolves(data.cfData.vcapData[0]);
+    service.discover([{invalidPin: 'role:test'}], { domain: 'mockUrl', name: '*', password: '*' },
+      (err, response) => {
+        expect(response).to.be.a('Map');
+      });
+  });
+
+  it('should return an exception  on invalid Url', () => {
+    cloudControllerStub.getInfo.rejects(new Error('Invalid Url'));
+    service.discover([{ pin: 'role:test' }], { domain: 'invalid', name: '*', password: '*' },
+      (err, response) => {
+        expect(err).to.be.instanceOf(Error);
       });
   });
 });
