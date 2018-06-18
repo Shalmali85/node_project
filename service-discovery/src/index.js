2108754b703c56b7780612671cd7dd516af00a64
d867421 <shalmali.roy@team.telstra.com>
2018-06-18 16:26:12 +1000

    Added test cases and error handling

--- service-discovery/src/index.js
+++ service-discovery/src/index.js
@@ -1,18 +1,29 @@
-const serviceDiscovery = require('./discover-service');
+const serviceDiscovery = require('./discover-service-pcf');
 const Cache = require('node-cache');
 
 const cache = new Cache({ stdTTL: 1800, checkperiod: 600, useClones: false });
 let opt = {};
+
+/**
+ * options ={domain,name,password of the cloudfoundry instance}
+ * @param options
+ */
 function service(options) {
   if (options) {
     opt = options;
   }
 }
 
+/**
+ * Creates a middleware and calls service discovery to locate services
+ * @param pins
+ * @param opt
+ * @returns {function(pins, options, callback)}
+ */
 function createMiddleWare(pins, opt) {
   return (req, res, next) => {
     if (!pins || !(pins instanceof Array)) {
-      return next(new Error('options must be an array'), null);
+      return next(new Error('patterns must be an array'), null);
     }
     pins.forEach((pattern) => {
       if (pattern && pattern.pin) {
@@ -21,25 +32,32 @@ function createMiddleWare(pins, opt) {
           return next(new Error('Not a valid pattern. Valid formats role:service name or cmd:service name '));
         }
       }
+      else {
+          return next(new Error('Pattern or Pin identifier missing '));
+      }
     });
     const cachedvalue = cache.get('services');
     if (cachedvalue) {
       req.clientUrl = cachedvalue;
       return next();
     }
-
     serviceDiscovery.discover(pins, opt, (err, response) => {
-      if (response) {
-        console.log(response);
+      if (response && response.size>0) {
         req.clientUrl = response;
         cache.set('services', response, 1800);
         return next();
       }
       return next(err);
+
     });
   };
 }
 
+/**
+ * The services to be located [{pin:'role:test'}]
+ * @param pin
+ * @returns {function(pins, options, callback)}
+ */
 service.prototype.find = function (pin) {
   return createMiddleWare(pin, opt);
 };
--- service-discovery/test/index.test.js
+++ service-discovery/test/index.test.js
@@ -1,54 +1,102 @@
 const chai = require('chai');
-const sinonChai = require('sinon-chai');
-const dirtyChai = require('dirty-chai');
 const sinon = require('sinon');
-const proxyquire = require('proxyquire');
-const testData = require('./billing.testdata.json');
-const service = require('../src/discover-service')
-const {describe,it}  = mocha;
-chai.should();
-chai.use(sinonChai);
-chai.use(dirtyChai);
+const serviceDiscovery = require('../src/discover-service-pcf');
+const MiddleWare = require('../src/index.js');
+const run = require('express-unit');
 
+const expect = chai.expect;
 const sandbox = sinon.sandbox.create();
-const args = {
-    accountId: '200034767920',
-};
-const config = {};
-describe('test service discovery middleware', () => {
-    let serviceStub;
-    let index;
-    beforeEach(() => {
-        serviceStub = sandbox.spy(() => new service());
+
+const options = { domain: 'mockUrl', name: '*', password: '*' };
+const pins = [{ pin: 'role:test' }];
+
+describe('MiddleWare test', () => {
+  let serviceDiscoveryStub;
+
+  function setupServiceStubs() {
+    serviceDiscoveryStub.discover = sandbox.stub(serviceDiscovery, 'discover');
+  }
 
 
-        index = proxyquire('../src/index.js', {
-            serviceDiscovery: serviceStub,
-        });
-        serviceStub.discover = sandbox.stub(service.prototype, 'discover');
+  beforeEach(() => {
+    serviceDiscoveryStub = sandbox.stub(serviceDiscovery, 'serviceDiscovery');
+    setupServiceStubs();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should return a map with matching services for a pattern', (done) => {
+    const map = new Map();
+    map.set('role:test', 'test.cfapps.io');
+    const middleWare = new MiddleWare(options);
+
+    const stub = serviceDiscoveryStub.discover.yields([pins, options], map).returns(map);
+    const callback = sinon.spy();
+    const response = stub(callback);
+    expect(response).to.be.a('Map');
+    run(null, middleWare.find(pins), done);
+  });
+  it('should return a map with matching services for a pattern even when options are not provided and read from manifest file instead', (done) => {
+    const map = new Map();
+    map.set('role:test', 'test.cfapps.io');
+    const middleWare = new MiddleWare();
+
+    const stub = serviceDiscoveryStub.discover.yields([pins, null], map).returns(map);
+    const callback = sinon.spy();
+    const response = stub(callback);
+    expect(response).to.be.a('Map');
+    run(null, middleWare.find(pins), done);
+  });
+});
+describe('MiddleWare exception test', () => {
+  let serviceDiscoveryStub;
+
+  function setupServiceStubs() {
+    serviceDiscoveryStub.discover = sandbox.stub(serviceDiscovery, 'discover');
+  }
+
+
+  beforeEach(() => {
+    serviceDiscoveryStub = sandbox.stub(serviceDiscovery, 'serviceDiscovery');
+    setupServiceStubs();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+  it('should return an error  when cannot connect to cloud foundry or services not found', (done) => {
+    const error = new Error('getaddrinfo ENOTFOUND mockUrl:443');
+    const middleWare = new MiddleWare(options);
+    const stub = serviceDiscoveryStub.discover.yields(error).returns(error);
+    const callback = sinon.spy();
+    const err = stub(callback);
+    expect(err).to.be.instanceOf(Error);
+    run(null, middleWare.find(pins), done);
+  });
+
+  it('should return an error  when pin not an array', (done) => {
+    const middleWare = new MiddleWare(options);
+    run(null, middleWare.find({ pin: 'role:test' }), (err) => {
+      expect(err).to.be.instanceOf(Error);
+      done();
     });
+  });
 
-    afterEach(() => {
-        sandbox.restore();
+  it('should return an error  when pin not in seneca specific format ', (done) => {
+    const middleWare = new MiddleWare(options);
+    run(null, middleWare.find([{ pin: 'test' }]), (err) => {
+      expect(err).to.be.instanceOf(Error);
+      done();
     });
+  });
 
-
-    it('should return a map of services with matching patterns', () => {
-        const map =new Map();
-        serviceStub.discover.callsFake(function fakeFn() {
-            map.set('role:test', {
-                host: 'test,cfapps.io',
-                port: 443,
-                protocol: 'https',
-                timeout: 60000
-            });
-            return map;
-        });
-
-        index(config, args, (err, result) => {
-            result.should.be.deep.equal(testData.responseData);
-        });
+  it('should return an error  when pattern is null or pin identifier missing ', (done) => {
+    const middleWare = new MiddleWare(options);
+    run(null, middleWare.find([{ pin: 'test' }, {}]), (err) => {
+      expect(err).to.be.instanceOf(Error);
+      done();
     });
-
-
+  });
 });
