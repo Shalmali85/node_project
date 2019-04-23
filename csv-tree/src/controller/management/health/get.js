b53ab2ef3c6d3c0b76725502ce6025aca7cc5052
Shalmali <royshalmali85@gmail.com>
2019-04-23 16:26:26 +0530

    deleted

--- csv-tree/src/controller/management/health/get.js
+++
@@ -1,9 +0,0 @@
-function handler(request, reply) {
-  return reply.sendJson({ status: 'UP' }, 200);
-}
-
-module.exports = {
-  handler,
-  insecure: true,
-};
-
--- csv-tree/src/middleware/cache-control.js
+++
@@ -1,6 +0,0 @@
-module.exports = function cacheControl(req, res, next) {
-  if (process.env.CACHE_CONTROL) {
-    res.header('Cache-Control', process.env.CACHE_CONTROL);
-  }
-  next();
-};
